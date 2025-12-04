1d4699d9a782ada3e4487a7f539a4123fa7cacc2
Kenneth Xu <kenships0402@gmail.com>
2025-12-04 02:34:55 -0500

    feature: toggle improved
    features: animation, editor time anchor updates, custom editor

--- Assets/Scripts/SlideToggle.cs
+++ Assets/Scripts/SlideToggle.cs
@@ -1,21 +1,23 @@
 using System;
+using PrimeTween;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class SlideToggle : Selectable, IPointerClickHandler, ISubmitHandler, ICanvasElement
 {
+    #region Serialized Fields
+    [SerializeField]
+    private bool m_isOn;
+
     [SerializeField]
     private GameObject toggleBall;
 
     [SerializeField]
-    private Image background;
-
-    [Tooltip("Set toggle to be off when on left side")]
-    [SerializeField]
-    private bool leftOff;
+    private Graphic background;
 
     [SerializeField]
     private Color onColor = Color.white;
@@ -24,71 +26,135 @@ public class SlideToggle : Selectable, IPointerClickHandler, ISubmitHandler, ICa
     private Color offColor = Color.gray;
 
     [SerializeField]
-    private UnityEvent<bool> onValueChanged;
+    private float animationTime = 0.3f;
 
+    [SerializeField]
+    private UnityEvent<bool> onValueChanged;
+    #endregion
+    
+    #region Public Properties
+    public bool IsOn
+    {
+        get => m_isOn;
+        set => SetIsOn(value);
+    }
+
+    private void SetIsOn(bool value, bool sendCallback = true)
+    {
+        if (m_isOn == value)
+            return;
+
+        m_isOn = value;
+
+        PlayEffect(false);
+
+        if (sendCallback)
+        {
+            UISystemProfilerApi.AddMarker("Toggle.value", this);
+            onValueChanged.Invoke(m_isOn);
+        }
+    }
+    #endregion
+    
+    #region private Properties
     private RectTransform m_toggleRectTransform;
     private RectTransform m_backgroundRectTransform;
 
     private float m_initialDelta;
-    private bool m_isOn;
 
     private float m_toggleStart;
     private float m_toggleEnd;
-
-    protected override void Start()
+    #endregion
+    
+    #region MonoBehaviour Functions
+    protected override void OnEnable()
     {
-        base.Start();
-        onValueChanged.AddListener(OnToggle);
-        
+        base.OnEnable();
         m_toggleRectTransform = toggleBall.GetComponent<RectTransform>();
         m_backgroundRectTransform = background.gameObject.GetComponent<RectTransform>();
+        SetAnchors();
+        PlayEffect(true);
+    }
+    protected override void Start()
+    {
+        PlayEffect(true);
+    }
+    #endregion
+    
+    #region Animation Control Methods
+    protected override void OnDidApplyAnimationProperties()
+    {
+        // Check if isOn has been changed by the animation.
+        // Unfortunately there is no way to check if we don�t have a graphic.
+        if (toggleBall)
+        {
+            bool oldValue = !Mathf.Approximately(m_toggleRectTransform.anchoredPosition.x, m_toggleEnd);
+            if (m_isOn != oldValue)
+            {
+                m_isOn = oldValue;
+                SetIsOn(!oldValue);
+            }
+        }
 
+        base.OnDidApplyAnimationProperties();
+    }
+
+    private void SetAnchors()
+    {
         float bgLeftAnchor = m_backgroundRectTransform.anchoredPosition.x -
                              (m_backgroundRectTransform.pivot.x * m_backgroundRectTransform.sizeDelta.x);
-        Debug.Log(bgLeftAnchor);
         float bgRightAnchor = m_backgroundRectTransform.anchoredPosition.x +
                               ((1 - m_backgroundRectTransform.pivot.x) * m_backgroundRectTransform.sizeDelta.x);
-        Debug.Log(bgRightAnchor);
         float toggleLeftAnchor = m_toggleRectTransform.anchoredPosition.x -
                                  (m_toggleRectTransform.pivot.x * m_toggleRectTransform.sizeDelta.x);
-        Debug.Log(toggleLeftAnchor);
-        float toggleRightAnchor = m_toggleRectTransform.anchoredPosition.x +
-                                  ((1 - m_toggleRectTransform.pivot.x) * m_toggleRectTransform.sizeDelta.x);
-        Debug.Log(toggleRightAnchor);
 
-        if (leftOff)
-        {
-            float delta = toggleLeftAnchor - bgLeftAnchor;
-            m_toggleStart = m_toggleRectTransform.anchoredPosition.x;
-            m_toggleEnd = bgRightAnchor
-                          - delta
-                          - ((1 - m_toggleRectTransform.pivot.x) * m_toggleRectTransform.sizeDelta.x);
-        }
-        else
-        {
-            float delta = bgRightAnchor - toggleRightAnchor;
-            Debug.Log(delta);
-            m_toggleStart = m_toggleRectTransform.anchoredPosition.x;
-            Debug.Log((m_backgroundRectTransform.pivot.x * m_backgroundRectTransform.sizeDelta.x));
-            m_toggleEnd = bgLeftAnchor
-                          + delta
-                          + (m_toggleRectTransform.pivot.x * m_toggleRectTransform.sizeDelta.x);
-        }
+        float delta = toggleLeftAnchor - bgLeftAnchor;
+        m_toggleStart = m_toggleRectTransform.anchoredPosition.x;
+
+        m_toggleEnd = bgRightAnchor - delta
+                                    - ((1 - m_toggleRectTransform.pivot.x) * m_toggleRectTransform.sizeDelta.x);
     }
 
-    private void OnToggle(bool value)
+    private void PlayEffect(bool instant)
     {
-        m_toggleRectTransform.anchoredPosition = value
-            ? new Vector2(m_toggleEnd, m_toggleRectTransform.anchoredPosition.y)
-            : new Vector2(m_toggleStart, m_toggleRectTransform.anchoredPosition.y);
-        
-        background.color = value ? onColor : offColor;
-    }
+#if UNITY_EDITOR
+        if (Tween.GetTweensCount(m_toggleRectTransform) == 0 &&
+            !Mathf.Approximately(m_toggleRectTransform.anchoredPosition.x, m_toggleStart) &&
+            !Mathf.Approximately(m_toggleRectTransform.anchoredPosition.x, m_toggleEnd))
+            SetAnchors();
+#endif
 
-    public override void OnPointerDown(PointerEventData eventData)
-    {
-        base.OnPointerDown(eventData);
+        Tween.StopAll();
+
+        Tween.UIAnchoredPositionX(
+            target: m_toggleRectTransform,
+            startValue: m_isOn ? m_toggleStart : m_toggleEnd,
+            endValue: m_isOn ? m_toggleEnd : m_toggleStart,
+            ease: Ease.InOutExpo,
+            duration: instant ? 0 : animationTime,
+            cycles: 1
+        ).Group(
+            Tween.Color(
+                target: background,
+                startValue: m_isOn ? offColor : onColor,
+                endValue: m_isOn ? onColor : offColor,
+                duration: instant ? 0 : animationTime,
+                ease: Ease.InOutExpo,
+                cycles: 1
+            ));
     }
+    #endregion
+    
+    #region Editor Only
+#if UNITY_EDITOR
+    protected override void OnValidate()
+    {
+        base.OnValidate();
+
+        if (!UnityEditor.PrefabUtility.IsPartOfPrefabAsset(this) && !Application.isPlaying)
+            CanvasUpdateRegistry.RegisterCanvasElementForLayoutRebuild(this);
+    }
+#endif
 
     public void Rebuild(CanvasUpdate executing)
     {
@@ -105,15 +171,15 @@ public class SlideToggle : Selectable, IPointerClickHandler, ISubmitHandler, ICa
     public void GraphicUpdateComplete()
     {
     }
-
+    #endregion
+    
+    #region Input Methods
     private void InternalToggle()
     {
         if (!IsActive() || !IsInteractable())
             return;
 
-        m_isOn = !m_isOn;
-        Debug.Log(m_isOn);
-        onValueChanged.Invoke(m_isOn);
+        IsOn = !IsOn;
     }
 
     public virtual void OnPointerClick(PointerEventData eventData)
@@ -128,4 +194,5 @@ public class SlideToggle : Selectable, IPointerClickHandler, ISubmitHandler, ICa
     {
         InternalToggle();
     }
+    #endregion
 }
