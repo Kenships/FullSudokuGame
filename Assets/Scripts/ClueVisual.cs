1fa44c4f48427c5f58de5c623cbc679aaea88728
Kenneth Xu <kenships0402@gmail.com>
2025-12-06 22:12:18 -0500

    feature: Animated Grid Unit added (not implemented)

--- Assets/Scripts/ClueVisual.cs
+++ Assets/Scripts/ClueVisual.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using DefaultNamespace;
 using Obvious.Soap;
+using PrimeTween;
 using SudokuLogic.Interface;
 using TMPro;
 using UnityEngine;
@@ -10,35 +11,64 @@ using UnityEngine.UI;
 public class ClueVisual : MonoBehaviour, ISudokuCell
 {
     #region Constants
+
     private const string MSPACE = "<mspace=0.5em>";
+
     #endregion
-    
+
     #region Serialized Fields
+
     [Header("References")]
-    [SerializeField] TextMeshProUGUI text;
-    [SerializeField] Image background;
-    [SerializeField] private ScriptableEventVector2Int onSelect;
+    [SerializeField]
+    TextMeshProUGUI text;
+
+    [SerializeField]
+    Image background;
+
+    [SerializeField]
+    GameObject animatedBackground;
+
+    [SerializeField]
+    private ScriptableEventVector2Int onSelect;
+
     [Space]
     [Header("Font Scale")]
-    [SerializeField] private int valueSize;
-    [SerializeField] private int noteSize;
+    [SerializeField]
+    private int valueSize;
+
+    [SerializeField]
+    private int noteSize;
+
     [Space]
     [Header("Grid Colors")]
-    [SerializeField] private Color defaultColor;
-    [SerializeField] private Color highlightColor;
-    [SerializeField] private Color selectColor;
-    [SerializeField] private Color defaultFontColor;
-    [SerializeField] private Color lockedFontColor;
+    [SerializeField]
+    private Color defaultColor;
+
+    [SerializeField]
+    private Color highlightColor;
+
+    [SerializeField]
+    private Color selectColor;
+
+    [SerializeField]
+    private Color defaultFontColor;
+
+    [SerializeField]
+    private Color lockedFontColor;
+
     #endregion
+
     public Vector2Int Position { get; private set; }
 
     public int Value { get; set; }
     public bool[] Notes { get; private set; }
 
-    
     private bool m_isLocked;
-    
+
     private bool[] m_boldNotes;
+
+    private RectTransform animatedBackgroundRect;
+    private Image animatedBackgroundImage;
 
     public void Initialize(Vector2Int position, int value, bool lockNumber = false)
     {
@@ -47,44 +77,48 @@ public class ClueVisual : MonoBehaviour, ISudokuCell
         Notes = new bool[9];
         m_boldNotes = new bool[9];
         m_isLocked = lockNumber;
+
+        animatedBackgroundRect = animatedBackground.GetComponent<RectTransform>();
+        animatedBackgroundImage = animatedBackground.GetComponent<Image>();
     }
 
     private void Start()
     {
+        animatedBackground.gameObject.SetActive(false);
         UpdateVisual();
     }
-    
+
     #region Update Values
 
     public void SetValue(int value, bool force = false)
     {
-        if(m_isLocked && !force)
+        if (m_isLocked && !force)
             return;
-        
+
         for (int i = 0; i < 9; i++)
         {
             Notes[i] = false;
         }
-        
+
         Value = value;
         UpdateVisual();
     }
 
     public void ToggleNote(int note, bool force = false)
     {
-        if(m_isLocked && !force)
+        if (m_isLocked && !force)
             return;
-        
+
         Value = 0;
         Notes[note - 1] = !Notes[note - 1];
         UpdateVisual();
     }
-    
+
     public void AddNote(int note, bool force = false)
     {
-        if(m_isLocked && !force)
+        if (m_isLocked && !force)
             return;
-        
+
         Value = 0;
         Notes[note - 1] = true;
         UpdateVisual();
@@ -92,13 +126,13 @@ public class ClueVisual : MonoBehaviour, ISudokuCell
 
     public void RemoveNote(int note, bool force = false)
     {
-        if(m_isLocked && !force)
+        if (m_isLocked && !force)
             return;
-        
+
         Notes[note - 1] = false;
         UpdateVisual();
     }
-    
+
     public void Erase(bool force = false)
     {
         SetValue(0, force);
@@ -115,9 +149,11 @@ public class ClueVisual : MonoBehaviour, ISudokuCell
         m_isLocked = false;
         UpdateVisual();
     }
+
     #endregion
 
     #region UpdateVisual
+
     public void UpdateVisual()
     {
         if (Value == 0 && IsNotesEmpty())
@@ -125,6 +161,7 @@ public class ClueVisual : MonoBehaviour, ISudokuCell
             text.gameObject.SetActive(false);
             return;
         }
+
         if (Value != 0)
         {
             text.fontSize = valueSize;
@@ -135,6 +172,7 @@ public class ClueVisual : MonoBehaviour, ISudokuCell
             text.fontSize = noteSize;
             text.text = NoteString();
         }
+
         text.color = m_isLocked ? lockedFontColor : defaultFontColor;
         text.gameObject.SetActive(true);
     }
@@ -158,8 +196,9 @@ public class ClueVisual : MonoBehaviour, ISudokuCell
 
     public void BoldNote(int note)
     {
-        if (!Notes[note - 1] || Value != 0) return;
-        
+        if (!Notes[note - 1] || Value != 0)
+            return;
+
         m_boldNotes[note - 1] = true;
         UpdateVisual();
     }
@@ -167,7 +206,7 @@ public class ClueVisual : MonoBehaviour, ISudokuCell
     public void UnboldNote(int note, bool update = true)
     {
         m_boldNotes[note - 1] = false;
-        
+
         if (update)
             UpdateVisual();
     }
@@ -178,23 +217,79 @@ public class ClueVisual : MonoBehaviour, ISudokuCell
         {
             UnboldNote(r, false);
         }
+
         UpdateVisual();
     }
 
     #endregion
-    
+
+    #region Animation
+
+    public void Animate()
+    {
+        animatedBackground.gameObject.SetActive(true);
+        Tween.UISizeDelta(
+                target: animatedBackgroundRect,
+                startValue: new Vector2(20, 20),
+                endValue: new Vector2(90, 90),
+                duration: 0.5f,
+                ease: Ease.OutExpo,
+                cycles: 1,
+                cycleMode: CycleMode.Rewind)
+            .Group(
+                Tween.Alpha(target: animatedBackgroundImage,
+                    startValue: 0f,
+                    endValue: 255f,
+                    duration: 0.5f,
+                    ease: Ease.OutExpo,
+                    cycles: 1,
+                    cycleMode: CycleMode.Rewind)
+            )
+            .Chain(
+                Tween.LocalRotation(
+                        target: animatedBackgroundRect,
+                        startValue: Vector3.zero,
+                        endValue: new Vector3(0, 0, 20),
+                        duration: 0.3f,
+                        ease: Ease.OutExpo,
+                        cycles: 2,
+                        cycleMode: CycleMode.Rewind
+                    )
+            ).Group(
+                Tween.UISizeDelta(
+                    target: animatedBackgroundRect,
+                    startValue: new Vector2(89, 89),
+                    endValue: new Vector2(20, 20),
+                    duration: 0.3f,
+                    ease: Ease.OutExpo,
+                    cycles: 1,
+                    cycleMode: CycleMode.Rewind)
+            ).Group(
+                Tween.Alpha(target: animatedBackgroundImage,
+                    startValue: 255f,
+                    endValue: 0f,
+                    duration: 0.2f,
+                    ease: Ease.OutExpo,
+                    cycles: 1,
+                    cycleMode: CycleMode.Rewind)
+                ).OnComplete(() => animatedBackground.SetActive(false));
+    }
+
+    #endregion
+
     #region Util
 
     private bool IsNotesEmpty()
     {
         for (int i = 0; i < 9; i++)
         {
-            if (Notes[i]) return false;
+            if (Notes[i])
+                return false;
         }
-        
+
         return true;
     }
-    
+
     private string NoteString()
     {
         string result = MSPACE;
@@ -202,17 +297,16 @@ public class ClueVisual : MonoBehaviour, ISudokuCell
         {
             if (Notes[i])
             {
-                result += m_boldNotes[i] ? "<b>" + (i + 1) + "</b>": i + 1;
+                result += m_boldNotes[i] ? "<b>" + (i + 1) + "</b>" : i + 1;
             }
             else
             {
                 result += "+";
             }
         }
-        
+
         return result;
     }
-    
 
     public bool HasNote(int note)
     {
